ed158af18c45f2da84d28ced6a19aeea2d0d3c27
JuyelRana <mjuyelrana@gmail.com>
2021-07-19 07:25:15 +0600

    function types && return types of function

--- output/typescript/script.js
+++ output/typescript/script.js
@@ -1,24 +1,7 @@
 "use strict";
-// Any type variables
-var a;
-a = 20;
-a = {
-    name: 'Rafia',
-    age: a,
-    occupation: 'Craft Instructor',
-    isMarried: false
+var myRafi;
+myRafi = function (name, age, occupation, isMarried) {
+    if (isMarried === void 0) { isMarried = false; }
+    return name + " " + age + " " + occupation + " " + isMarried;
 };
-console.log(a);
-a = "Juyel Rana";
-console.log(a);
-var b = [];
-b.push(1);
-b.push('Rafia');
-b.push(26);
-b.push({
-    name: 'Rafia',
-    age: 26,
-    occupation: 'Craft Instructor',
-    isMarried: false
-});
-console.log(b);
+console.log(myRafi('Rafia Rahman', 26, 'Craft Intsructor'));
--- src/typescript/script.ts
+++ src/typescript/script.ts
@@ -1,33 +1,9 @@
-// Any type variables
-let a: any;
+let myRafi: Function;
 
-a = 20;
-
-
-a = {
-    name: 'Rafia',
-    age: a,
-    occupation: 'Craft Instructor',
-    isMarried: false
+myRafi = (name: string, age: number, occupation: string, isMarried: boolean = false): string => {
+    return name + " " + age + " " + occupation + " " + isMarried;
 }
 
-console.log(a);
-
-a = "Juyel Rana";
-
-console.log(a);
+console.log(myRafi('Rafia Rahman', 26, 'Craft Intsructor'));
 
 
-let b: any[] = [];
-
-b.push(1);
-b.push('Rafia');
-b.push(26);
-b.push({
-    name: 'Rafia',
-    age: 26,
-    occupation: 'Craft Instructor',
-    isMarried: false
-});
-
-console.log(b);
